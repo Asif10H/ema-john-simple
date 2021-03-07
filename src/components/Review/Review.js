510beb8a97299da84943e372c42e95ad940d5ffb
Asif10H <habibullahasif71@gmail.com>
2021-03-07 15:18:07 +0600

    added all file

---
+++ src/components/Review/Review.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import Cart from '../Cart/Cart';
+import happyImape from '../../images/giphy.gif';
+const Review = () => {
+    const [cart, setCart] = useState([]);
+    const [orderPlace, setOrderPlace] = useState(false);
+
+    const handlePlaceOrder = () => {
+        setCart([]);
+        setOrderPlace(true);
+        processOrder();
+     }
+
+    const removeProduct = (productKey) => {
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
+
+    useEffect(() => {
+        //cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        }, []);
+        setCart(cartProducts);
+    }, []);
+    let thankyou;
+    if(orderPlace){
+        thankyou = <img src={happyImape} alt=""/>
+    } 
+    return (
+        <div className="twin-container">
+            <div className="product-container">
+                {
+                    cart.map(pd => <ReviewItem
+                        key={pd.key}
+                        removeProduct={removeProduct}
+                        product={pd}></ReviewItem>)
+                }
+                {
+                    thankyou
+                }
+            </div>
+            <div className="cart-container">
+               <Cart cart={cart}>
+                   <button onClick = {handlePlaceOrder}  className="main-button">Place Order</button>
+               </Cart>
+            </div>
+
+        </div>
+    );
+};
+
+export default Review;
